d157c68e36d2bca8edba48c282206249f419a498
thomaspizzelli-alt <thomas.pizzelli@hotmail.com>
2026-02-23 15:41:38 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rapport-intervention-v6"; // <-- change à chaque update
+const CACHE_NAME = "rapport-intervention-v7"; // <-- change à chaque update
 const STATIC_ASSETS = [
   "./",
   "./manifest.webmanifest",
@@ -46,3 +46,4 @@ self.addEventListener("fetch", (event) => {
   );
 
 });
+
